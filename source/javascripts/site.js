5bc22e43619c24bdb1de0c5bcbff396bf79baf5a
Sebastian Graz <sebastian.graz@spindriftgroup.com>
2017-11-29 08:41:51 +0100

    added weight slider, infinite text scroll

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,12 +1,34 @@
-// function setScaledFont(){
-//     var el = document.getElementsByClassName("fill");
-//     for(var i = 0; i < el.length; i++){
-//       var s  = el[i].offsetWidth,
-//           f  = 0.24,
-//           fs = s * f;
-//       el[i].style.fontSize = fs + '%'
-//     }
-// } setScaledFont()
+function setScaledFont(){
+    var el = document.getElementsByClassName("fill");
+    for(var i = 0; i < el.length; i++){
+      var s  = el[i].offsetWidth,
+          f  = 0.24, //0.24
+          fs = s * f;
+      el[i].style.fontSize = fs + '%'
+    }
+}
+
+
+
+// function recalcTextRepeat() {
+//   var expandElem = document.querySelectorAll('.intro-questions > .intro-question > .intro-question-text')
+//   var totalWidth = 0;
+//
+//   for(var i = 0, l = expandElem.length; i < l; i++) {
+//     // console.log(expandElem[i].getBoundingClientRect().width);
+//     // var text = child[i].innerHTML.repeat(1);
+//     // child[i].insertAdjacentHTML('beforeend', text)
+//     var fits = Math.floor( window.innerWidth / expandElem[i].offsetWidth )
+//     // console.log(expandElem[i]);
+//     console.log(  expandElem[i].offsetWidth % window.innerWidth + 'hey');
+//
+//     var text = expandElem[i].innerHTML.repeat(fits);
+//     expandElem[i].insertAdjacentHTML('beforeend', text)
+//
+//   }
+// }
+
+
 
 function hero(scroll_pos) {
   var el = document.getElementsByClassName("black-shiny");
@@ -29,21 +51,13 @@ function parallax() {
 
 
 window.onresize = function () {
-    setScaledFont()
+  setScaledFont()
 }
 
 window.addEventListener('scroll', function(e) {
   parallax()
 });
 
-function getTextWidth(text, font) {
-    // re-use canvas object for better performance
-    var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
-    var context = canvas.getContext("2d");
-    context.font = font;
-    var metrics = context.measureText(text);
-    return metrics.width;
-}
 
 
 Array.prototype.sum = function(selector) {
@@ -59,31 +73,27 @@ Array.prototype.sum = function(selector) {
     return sum;
 };
 
-var expandElem = document.querySelector('#hero')
-var child = expandElem.children;
-var totalWidth = 0;
 
-for(var i = 0, l = child.length; i < l; i++) {
-  var text = child[i].innerHTML.repeat(1);
-  child[i].insertAdjacentHTML('beforeend', text)
-}
-
-let totalWidths = Array.from(
-  document.querySelectorAll('.nowrap'),
-  nowrap => Array.from(
-    nowrap.children,
-    child => child.offsetWidth
-  ).reduce((total, width) => total + width)
-)
+// let totalWidths = Array.from(
+//   document.querySelectorAll('.intro-question'),
+//   nowrap => Array.from(
+//     nowrap.children,
+//     child => child.offsetWidth
+//   ).reduce((total, width) => total + width)
+// )
+// console.log(totalWidths)
 
 
-for (var i = 0; i < totalWidths.length; i++) {
-  var fits = Math.floor( window.innerWidth / totalWidths[i] )
-  if (fits > 0) {
-    var text = child[i].innerHTML.repeat(fits);
-    child[i].insertAdjacentHTML('beforeend', text)
-  }
-}
+
+// for (var i = 0; i < totalWidths.length; i++) {
+//   console.log(totalWidths[i]);
+//   var fits = Math.floor( window.innerWidth / totalWidths[i] )
+//   console.log(expandElem[i]);
+//   if (fits > 0) {
+//     var text = expandElem[i].innerHTML.repeat(fits);
+//     expandElem[i].insertAdjacentHTML('beforeend', text)
+//   }
+// }
 //
 // $('#hero').each(function(){
 //   var child = $(this).children();
@@ -123,6 +133,105 @@ for (var i = 0; i < totalWidths.length; i++) {
 //     charset.classList.toggle(weightItem);
 //   }, false);
 // }
+
+var range = document.querySelector('#myRange');
+var weightParent = document.querySelector('.intro-questions');
+
+
+range.oninput = function() {
+    weightParent.className = "intro-questions";
+    switch (parseInt(this.value)) {
+      case 1:
+        weightParent.classList.add('fw2');
+        break;
+      case 2:
+        weightParent.classList.add('fw3');
+        break;
+      case 4:
+        weightParent.classList.add('fw5');
+        break;
+      case 5:
+        weightParent.classList.add('fw6');
+        break;
+      default:
+        weightParent.classList.add('fw4');
+    }
+}
+
+var textCopy = document.querySelectorAll('.intro-question-text')
+for (var i = 0; i < textCopy.length; i++) {
+  var text = textCopy[i].innerHTML.repeat(5)
+  textCopy[i].insertAdjacentHTML('beforeend', text)
+}
+
+var WIN_H,
+    WIN_W,
+    qFrame = [];
+
+var questions = document.querySelectorAll('.intro-question');
+var scrollRequest;
+var qScrollPositions = [];
+var passiveRepeaters = [];
+var repeater;
+
+function initIntroScript() {
+  Array.prototype.map.call(questions, function(q, i) {
+    addImageHover(i);
+  })
+  window.addEventListener('resize', resizeHandler);
+  resizeHandler();
+}
+
+function addImageHover(i) {
+  qFrame[i] = 0;
+  qScrollPositions[i] = Math.floor(Math.random() * -200);
+  questions[i].querySelector('.intro-question-text').style.transform = 'translate3d(' + qScrollPositions[i] + 'px,0,0)';
+  questions[i].addEventListener('mouseenter', function() {
+    // console.log("mouseenter -> " + i);
+
+    clearInterval(passiveRepeaters[i]);
+    repeater = setInterval(function() {
+      scrollQuestionText(i);
+
+    }, .001);
+  });
+
+  questions[i].addEventListener('mouseleave', function() {
+    //console.log("mouseleave -> " + i);
+    clearInterval(repeater);
+    passiveRepeaters[i] = setInterval(function() {
+
+      scrollQuestionText(i);
+    }, 40);
+  });
+
+  passiveRepeaters[i] = setInterval(function() {
+  	scrollQuestionText(i);
+  }, 40);
+}
+
+function scrollQuestionText(i) {
+  var shift = Math.floor(4 + WIN_W/800) * Math.min(0.2, qFrame[i] / 10);
+  cancelAnimationFrame(scrollRequest);
+  var el_text = questions[i].querySelector('.intro-question-text');
+  qScrollPositions[i] = qScrollPositions[i] - shift;
+  if (qScrollPositions[i] < -el_text.clientWidth / 2 - 5) {
+    qScrollPositions[i] = 0;
+  }
+  el_text.style.transform = 'translate3d(' + qScrollPositions[i] + 'px,0,0)'
+  qFrame[i]++;
+}
+
+initIntroScript();
+
+function resizeHandler() { // NEEDS TO NOT BREAK ON RESIZE
+  WIN_W = window.innerWidth;
+  WIN_H = window.innerHeight;
+}
+
+
+
+
 
 function randomDia() {
   var text = "";
